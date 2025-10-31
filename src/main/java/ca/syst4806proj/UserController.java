0e2981024d0dd5433c0c1408633de3b1be4b6f87
Samson Ha <whiteram81@gmail.com>
2025-10-30 21:05:45 -0400

    Add UserController create and get by idea features

---
+++ src/main/java/ca/syst4806proj/UserController.java
@@ -0,0 +1,24 @@
+package ca.syst4806proj;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping
+    public User createUser() {
+        return userRepository.save(new User());
+    }
+
+    @GetMapping("/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found with id " + id)) ;
+    }
+}
