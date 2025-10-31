5520ed6a9b0809867c2922f6758c8cc41958310b
Samson Ha <whiteram81@gmail.com>
2025-10-30 21:11:07 -0400

    Add get/setters for name attribute, constructor for just name param, id as generated value @Table(name = "users") this line is to make it so its default name "user" isnt being auto generated, will cause error

--- src/main/java/ca/syst4806proj/User.java
+++ src/main/java/ca/syst4806proj/User.java
@@ -3,10 +3,10 @@ package ca.syst4806proj;
 import jakarta.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class User {
-
-
     @Id
+    @GeneratedValue
     private Long id;
     private String name;
 
@@ -18,11 +18,22 @@ public class User {
 
     }
 
+    public User(String name) {
+        this.name = name;
+    }
+
     public void setId(Long id) {
         this.id = id;
     }
 
     public Long getId() {
         return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
     }
 }
