6ab08dcd4c7193b84544bc88899a44f2e38ffc69
Abdelrahman Elsayed <Abdelrahmanelsayed482@gmail.com>
2025-11-02 14:22:47 -0500

    Add StartupTest for backend verification

---
+++ src/test/java/ca/syst4806proj/StartupTest.java
@@ -0,0 +1,19 @@
+package ca.syst4806proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class StartupTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void appStartsAndSeedsUsers() {
+        assertThat(userRepository.count()).isGreaterThanOrEqualTo(4);
+    }
+}
