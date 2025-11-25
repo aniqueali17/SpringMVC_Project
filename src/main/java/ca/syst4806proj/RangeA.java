7769d8254a65d9416cad64956408eb441fd8189c
Samson Ha <whiteram81@gmail.com>
2025-11-25 13:05:26 -0500

    Add RangeQ - no ui

---
+++ src/main/java/ca/syst4806proj/RangeA.java
@@ -0,0 +1,44 @@
+package ca.syst4806proj;
+
+import jakarta.persistence.*;
+
+public class RangeA {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    public RangeQ rangeQ;
+
+    public int answer;
+
+    public RangeA() {}
+
+    public RangeA(int answer) {
+        this.answer = answer;
+    }
+
+    public void setAnswer(int answer) {
+        this.answer = answer;
+    }
+
+    public int getAnswer() {
+        return answer;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public RangeQ getQuestion() {
+        return rangeQ;
+    }
+
+    public void setQuestion(RangeQ question) {
+        this.rangeQ = question;
+    }
+}
